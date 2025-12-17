5066dd32c37fffb0d36a2571778e43e4c43aeb7c
folksheesh <zenalaydrusss@gmail.com>
2025-12-17 17:41:14 +0700

    fix bug

---
+++ resources/views/vendor/pagination/custom.blade.php
@@ -0,0 +1,46 @@
+@if ($paginator->hasPages())
+    <nav class="pagination-nav" aria-label="Pagination Navigation">
+        <ul class="pagination-list">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="pagination-item disabled" aria-disabled="true" aria-label="Sebelumnya">
+                    <span aria-hidden="true">&laquo;</span>
+                </li>
+            @else
+                <li class="pagination-item">
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="Sebelumnya">&laquo;</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="pagination-item disabled" aria-disabled="true"><span>{{ $element }}</span></li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="pagination-item active" aria-current="page"><span>{{ $page }}</span></li>
+                        @else
+                            <li class="pagination-item"><a href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="pagination-item">
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="Berikutnya">&raquo;</a>
+                </li>
+            @else
+                <li class="pagination-item disabled" aria-disabled="true" aria-label="Berikutnya">
+                    <span aria-hidden="true">&raquo;</span>
+                </li>
+            @endif
+        </ul>
+    </nav>
+@endif
